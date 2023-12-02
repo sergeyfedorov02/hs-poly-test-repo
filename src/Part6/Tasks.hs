31fcb2d6d320cf8198cd6b2e566fd3cdb7eb86ff
sergeyfedorov02 <sergey_fedorov200001@mail.ru>
2023-12-03 00:14:50 +0300

    Part6 done

--- src/Part6/Tasks.hs
+++ src/Part6/Tasks.hs
@@ -14,22 +14,39 @@ data SparseMatrix a = SparseMatrix {
 -- Определите класс типов "Матрица" с необходимыми (как вам кажется) операциями,
 -- которые нужны, чтобы реализовать функции, представленные ниже
 class Matrix mx where
+    zero :: Int -> Int -> mx
+    eye ::  Int -> mx
 
 -- Определите экземпляры данного класса для:
 --  * числа (считается матрицей 1x1)
 --  * списка списков чисел
 --  * типа SparseMatrix, представленного выше
 instance Matrix Int where
+    zero _ _ = 0
+    eye _ = 1
+
 instance Matrix [[Int]] where
+    -- Создаем матрицу нулей размера w x h
+    -- replicate - создание списка определенной длины из повторяющихся элементов
+    zero w h = replicate h (replicate w 0)
+    -- Создаем единичную матрицу размера w x w
+    eye w = [ [if i == j then 1 else 0
+            | j <- [0..w-1]]
+            | i <- [0..w-1] ]
+
 instance Matrix (SparseMatrix Int) where
+    -- Создаем пустую разреженную матрицу размера w x h
+    zero w h = SparseMatrix w h Data.Map.empty
+    -- Создаем единичную разреженную матрицу размера
+    eye w = SparseMatrix w w $ Data.Map.fromList [((i, i), 1) | i <- [0..w-1]]
 
 -- Реализуйте следующие функции
 -- Единичная матрица
-eye :: Matrix m => Int -> m
-eye w = notImplementedYet
+--eye :: Matrix m => Int -> m
+--eye w = notImplementedYet
 -- Матрица, заполненная нулями
-zero :: Matrix m => Int -> Int -> m
-zero w h = notImplementedYet
+--zero :: Matrix m => Int -> Int -> m
+--zero w h = notImplementedYet
 -- Перемножение матриц
 multiplyMatrix :: Matrix m => m -> m -> m
 multiplyMatrix = notImplementedYet

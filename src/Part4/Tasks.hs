f4e299ad01c2248a18707cfc8c34377478cf970f
sergeyfedorov02 <sergey_fedorov200001@mail.ru>
2023-12-02 18:32:32 +0300

    Part4 done

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -16,17 +16,44 @@ rlistToList lst =
 
 -- Реализуйте обратное преобразование
 listToRlist :: [a] -> ReverseList a
-listToRlist = notImplementedYet
+listToRlist = create REmpty
+    where
+      create acc [] = acc
+      create acc (x:xs) = create (acc :< x) xs
 
 -- Реализуйте все представленные ниже классы (см. тесты)
-instance Show (ReverseList a) where
-    showsPrec = notImplementedYet
-    show = notImplementedYet
-instance Eq (ReverseList a) where
-    (==) = notImplementedYet
-    (/=) = notImplementedYet
+instance Show a => Show (ReverseList a) where
+    show x = "[" ++ shows x ++ "]"
+      where
+        shows REmpty = ""
+        shows (REmpty :< x) = show x
+        shows (xs :< x) = shows xs ++ "," ++ show x
+
+instance Eq a => Eq (ReverseList a) where
+    (==) REmpty REmpty = True
+    (==) (xs :< x) (ys :< y) = (x == y) && (xs == ys)
+    (==) _ _ = False
+    (/=) x y = not (x == y)
+
 instance Semigroup (ReverseList a) where
+    xs <> REmpty = xs
+    REmpty <> ys = ys
+    xs <> (ys :< y) = (xs <> ys) :< y
+
 instance Monoid (ReverseList a) where
+    mempty = REmpty
+
 instance Functor ReverseList where
+    fmap _ REmpty = REmpty
+    fmap f (xs :< x) = fmap f xs :< f x
+
 instance Applicative ReverseList where
+    pure x = REmpty :< x
+    REmpty <*> _ = REmpty
+    _ <*> REmpty = REmpty
+    (fs :< f) <*> xs = (fs <*> xs) <> (fmap f xs)
+
 instance Monad ReverseList where
+    return = pure
+    REmpty >>= _ = REmpty
+    (xs :< x) >>= f = (xs >>= f) <> (f x)
